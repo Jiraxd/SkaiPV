74954cc515886aabb904a8e67621bf3d3aa950bf
Jira <123981279+Jiraxd@users.noreply.github.com>
2023-12-12 08:51:10 +0100

    Update page.tsx

--- app/[...profile]/page.tsx
+++ app/[...profile]/page.tsx
@@ -1,16 +1,18 @@
 import { MainProfilePage } from "@/components/MainProfilePage";
 import type { Metadata, ResolvingMetadata } from "next";
 
-export async function generateMetadata({
-  params,
-}: {
-  params: { profile: string[] };
-  parent: ResolvingMetadata;
-}): Promise<Metadata> {
-  const returnValue = params.profile[1];
+type Props = {
+  params: { profile: string[] }
+}
+
+export async function generateMetadata(
+  { params}: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+ const returnValue = params.profile[1];
   return {
     title: returnValue,
-  };
+  }
 }
 
 export default function displaydata({
